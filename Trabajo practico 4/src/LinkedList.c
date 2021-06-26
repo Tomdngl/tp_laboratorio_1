624de852ace86a18f553b3dccc7f11c4e5313f63
Tomas D'Angelo <tomidngl@gmail.com>
2021-06-26 11:30:36 -0300

    TP 4 - isEmpty

--- Trabajo practico 4/src/LinkedList.c
+++ Trabajo practico 4/src/LinkedList.c
@@ -97,7 +97,7 @@ static int addNode(LinkedList* this, int nodeIndex,void* pElement)
     Node* newNode = (Node*) malloc(sizeof(Node));
     Node* previousNode;
 
-    if(newNode != NULL && this != NULL  && nodeIndex >= 0)
+    if(newNode != NULL && this != NULL  && nodeIndex >= 0 && nodeIndex <= ll_len(this))
       {
       	newNode->pElement = pElement;
     	newNode->pNextNode = NULL;
@@ -115,6 +115,11 @@ static int addNode(LinkedList* this, int nodeIndex,void* pElement)
 		this->size++;
       	returnAux = 0;
       }
+    else
+    {
+    	free(newNode);
+    }
+
     return returnAux;
 }
 
@@ -140,12 +145,12 @@ int test_addNode(LinkedList* this, int nodeIndex,void* pElement)
                         ( 0) Si funciono correctamente
  *
  */
-int ll_add(LinkedList* this, void* pElement)
+int ll_add(LinkedList* pList, void* pElement)
 {
     int returnAux = -1;
-	if(this!=NULL)
+	if(pList!=NULL)
 	{
-		if(addNode(this, ll_len(this), pElement)== 0)
+		if(addNode(pList, ll_len(pList), pElement) == 0)
 		{
 			returnAux = 0;
 		}
@@ -154,19 +159,26 @@ int ll_add(LinkedList* this, void* pElement)
     return returnAux;
 }
 
-/** \brief Permite realizar el test de la funcion addNode la cual es privada
+/** \brief Retorna un puntero al elemento que se encuentra en el indice especificado.
  *
  * \param this LinkedList* Puntero a la lista
- * \param nodeIndex int Ubicacion del elemento a obtener
+ * \param index int Ubicacion del elemento a obtener
  * \return void* Retorna    (NULL) Error: si el puntero a la lista es NULL o (si el indice es menor a 0 o mayor al len de la lista)
                             (pElement) Si funciono correctamente
  *
  */
 void* ll_get(LinkedList* this, int index)
 {
-    void* returnAux = NULL;
+    Node* NodeAux = NULL;
+    void* pElement = NULL;
 
-    return returnAux;
+	NodeAux = getNode(this, index);
+	if(NodeAux != NULL  && index >= 0 && index <= ll_len(this))
+	{
+		pElement = NodeAux->pElement;
+	}
+
+    return pElement;
 }
 
 
@@ -183,6 +195,17 @@ int ll_set(LinkedList* this, int index,void* pElement)
 {
     int returnAux = -1;
 
+    Node* NodeAux = NULL;
+
+    if(this!=NULL && index >= 0 && index < ll_len(this))
+    {
+    	NodeAux = getNode(this, index);
+    	if(NodeAux != NULL)
+    	{
+    		NodeAux->pElement=pElement;
+			returnAux=0;
+    	}
+    }
     return returnAux;
 }
 
@@ -199,6 +222,31 @@ int ll_remove(LinkedList* this,int index)
 {
     int returnAux = -1;
 
+    Node* NodeAux;
+    Node* previousNode;
+    Node* nextNode;
+
+    NodeAux = getNode(this, index);
+    nextNode = getNode(this, index + 1);
+    previousNode = getNode(this, index - 1);
+
+    if(NodeAux != NULL)
+    {
+		if(index == 0)
+		{
+			this->pFirstNode = nextNode;
+			free(NodeAux);
+			this->size--;
+			returnAux = 0;
+		}
+		else if(previousNode != NULL)
+		{
+			previousNode->pNextNode = nextNode;
+			free(NodeAux);
+			this->size--;
+			returnAux = 0;
+		}
+	}
     return returnAux;
 }
 
@@ -214,6 +262,18 @@ int ll_clear(LinkedList* this)
 {
     int returnAux = -1;
 
+    if(this != NULL)
+    {
+    	for(int i = 0; i < ll_len(this); i++)
+    	{
+    		ll_remove(this, i);
+    		returnAux = 0;
+    	}
+    	if(ll_len(this) == 0)
+    	{
+    		returnAux = 0;
+    	}
+    }
     return returnAux;
 }
 
@@ -228,6 +288,12 @@ int ll_clear(LinkedList* this)
 int ll_deleteLinkedList(LinkedList* this)
 {
     int returnAux = -1;
+
+    if(this != NULL && ll_clear(this) == 0)
+    {
+		free(this);
+		returnAux = 0;
+    }
 
     return returnAux;
 }
@@ -244,6 +310,20 @@ int ll_indexOf(LinkedList* this, void* pElement)
 {
     int returnAux = -1;
 
+    Node* NodeAux;
+
+    if(this != NULL)
+    {
+    	for(int i = 0; i < ll_len(this); i++)
+    	{
+    		NodeAux = getNode(this, i);
+    		if(NodeAux->pElement == pElement)
+    		{
+    			returnAux = i;
+    			break;
+    		}
+    	}
+    }
     return returnAux;
 }
 
@@ -259,6 +339,17 @@ int ll_isEmpty(LinkedList* this)
 {
     int returnAux = -1;
 
+    if(this != NULL)
+    {
+    	if(ll_len(this) == 0)
+    	{
+    		returnAux = 1;
+    	}
+    	else
+    	{
+    		returnAux = 0;
+    	}
+    }
     return returnAux;
 }
 
